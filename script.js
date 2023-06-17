37096df46b7b319325ff5fa6b9e5bc91dbbdb961
Chandan Kumar Parida <120441215+ChandanP01@users.noreply.github.com>
2023-06-17 20:33:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Game Constants & variables
 let inputDir = { x: 0, y: 0 }
 const foodSound = new Audio('music/food.mp3')
-const gameOver = new Audio('/music/gameover.mp3')
-const moveSound = new Audio('/music/move.mp3')
-const musicSound = new Audio('/music/music.mp3')
+const gameOver = new Audio('music/gameover.mp3')
+const moveSound = new Audio('music/move.mp3')
+const musicSound = new Audio('music/music.mp3')
 let speed = 5
 let lastPaintTime = 0
 let snakeArr = [{ x: 13, y: 15 }]
